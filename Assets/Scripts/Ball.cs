48a0f47ef2ebafa71385e643acd980c31b63a625
Alejandro Poggi <Alejandro Poggi>
2016-11-21 14:42:36 -0500

    uploaded brick breaker

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	private Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector3;
+
+	// Use this for initialization
+	void Start ()
+	{
+		paddle = GameObject.FindObjectOfType<Paddle> ();
+		paddleToBallVector3 = this.transform.position - paddle.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!hasStarted) {
+			//lock ball to paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector3;
+		
+		
+			//launch ball
+			if (Input.GetMouseButtonDown (0)) {
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+				hasStarted = true;
+			}
+		}	
+	}
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		Vector2 tweak = new Vector2 (Random.Range (0f, 0.2f), Random.Range (0f, 0.2f));
+		if (hasStarted) {
+			audio.Play ();
+		}
+		rigidbody2D.velocity += tweak;
+	}
+}
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour
+{
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	private bool isBreakable;
+	private int timesHit;
+	private LevelManager levelManager;
+	public GameObject smoke;
+
+	bool NoSprite ()
+	{
+		if (this.GetComponent<SpriteRenderer> ().sprite == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	void Start ()
+	{
+		if (NoSprite ()) {
+			Debug.LogError ("No sprite attached to bricks");
+		}
+		isBreakable = (this.tag == "breakable");
+		//keep track of breakable bricks
+		if (isBreakable) {
+			breakableCount++;
+		}
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+		timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		AudioSource.PlayClipAtPoint (crack, transform.position, .03f);
+		if (isBreakable) {
+			HandleHits ();
+		}
+	}
+	
+	void HandleHits ()
+	{
+		int maxHits;
+		timesHit++;
+		maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			GameObject smokePuff = (GameObject)Instantiate (smoke, this.transform.position, Quaternion.identity);
+			smokePuff.particleSystem.startColor = this.GetComponent<SpriteRenderer> ().color;
+			breakableCount--;
+			levelManager.BrickDestroyed ();
+			Destroy (gameObject);
+		} else {
+			LoadSprites ();
+		}
+	}
+	
+	void LoadSprites ()
+	{
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
+	}
+	
+}
